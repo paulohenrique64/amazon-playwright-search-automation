844e39d360efa9c560eb97f4a6c1df8a896f9491
Paulo Henrique Ribeiro Alves <paulohenriquelvs20@gmail.com>
2024-03-16 16:56:19 -0300

    changed the way of viewing automation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const fs = require('fs');
 
+const fs = require('fs');
 const playwright = require('playwright');
 const { chromium, firefox, webkit } = require('playwright');
 
@@ -9,27 +9,25 @@ async function main() {
     const search_keyword = process.argv[2];
     let browser;
 
-    if (!search_keyword) {
-        console.log("Usage: npm run start 'Keyword'");
-        return;
-    }
-
+    if (!search_keyword) 
+        return console.log("Usage: npm run start 'Keyword'");
+        
     try {
         browser = await browserConnection();
     } catch (error) {
-        console.error(error);
-        return;
+        return console.error(error);
     }
+
 
     // opening amazon website and setting location
     const page = await browser.newPage();
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     
     let reloadPage = true;
     while (reloadPage) {
         await page.goto('https://www.amazon.com', { timeout: 2 * 60 * 1000});
         console.log('Amazon website opened...');
-        await pauseAndScreenshot(page);
+        await page.waitForTimeout(1000);
 
         try {
             await page.locator('#nav-global-location-popover-link').click();
@@ -40,20 +38,22 @@ async function main() {
         }
     }
 
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     await page.locator('.GLUX_Full_Width').fill("11001");
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     await page.locator('[class="a-column a-span4 a-span-last"]').getByText('Apply').click()
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     await page.click('.a-popover-footer > .a-button.a-column.a-button-primary.a-button-span4 > .a-button-inner.a-declarative');
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     console.log('Location 11001 selected...');
 
-    // search
+ 
+    // searching
     await page.fill('#twotabsearchtextbox', search_keyword);
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
     await page.click('#nav-search-submit-button');
-    await pauseAndScreenshot(page);
+    await page.waitForTimeout(1000);
+    console.log(`Searching for ${search_keyword}...`);
 
     let founded = false;
     for (const product of await page.locator('[class*=s-result-item][class*=s-asin]').all()) {
@@ -62,25 +62,26 @@ async function main() {
             founded = true;
 
             await product.locator('[class="s-image"]').first().click();
-            await pauseAndScreenshot(page);
+            await page.waitForTimeout(1000);
             break;
         }
     }
 
-    if (!founded) {
-        console.log("Product not founded!");
-        return;
-    }
+    if (!founded) 
+        return console.log("Product not founded!");
+
 
     // in product page
     const productData = await collectProductData(page);
     await showSearchResult(productData);
     saveInCsv(productData);
     
+
     // close browser
     await browser.close();
 }
 
+// make connection with browser set in .env file
 async function browserConnection() {
     const browser_name = process.env.PLAYWRIGHT_BROWSER;
     console.log(`Connection to scrape ${browser_name} browser...`);
@@ -88,13 +89,13 @@ async function browserConnection() {
     let browser;
     switch (browser_name) {
         case 'chromium':
-            browser = await chromium.launch();
+            browser = await chromium.launch({ headless: false });
             break;
         case 'firefox':
-            browser = await firefox.launch();
+            browser = await firefox.launch({ headless: false });
             break;
         case 'webkit':
-            browser = await webkit.launch();
+            browser = await webkit.launch({ headless: false });
             break;
         default:
             throw new Error(`Unsupported browser: ${browserName}`);
@@ -104,6 +105,7 @@ async function browserConnection() {
     return browser;
 }
 
+// collect product data of amazon page with handle exception
 async function collectProductData(page) {
     console.log("Colleting data, please wait...");
 
@@ -127,7 +129,7 @@ async function collectProductData(page) {
 
     // price
     try {
-        await page.waitForSelector('#corePriceDisplay_desktop_feature_div', {timeout: 7000})
+        await page.waitForSelector('#corePriceDisplay_desktop_feature_div', {timeout: 2000})
         productData.price = await page
             .locator('#corePriceDisplay_desktop_feature_div')
             .locator('[class="aok-offscreen"]')
@@ -180,35 +182,27 @@ async function collectProductData(page) {
 }
 
 async function showSearchResult(productData) {
-    console.log("\n");
+    console.log(`\nProduct data:\nTitle: ${productData.title}\nPrice: ${productData.price}\nBoughts: ${productData.boughts}\n`);
 
-    if (productData.title) console.log(`Title: ${productData.title}\n`);
-    if (productData.price) console.log(`Price: ${productData.price}\n`);
-    if (productData.boughts) console.log(`Boughts: ${productData.boughts}\n`);
     if (productData.bulletPoints) {
+        console.log('Bullet points: \n');
         for (const bulletPoint of productData.bulletPoints) 
-            console.log(`* ${await bulletPoint.textContent()}\n`);
+            console.log(`*${await bulletPoint.textContent()}`);
     }
-}
-
-async function pauseAndScreenshot(page) {
-    await page.waitForTimeout(1000);
-    await page.screenshot({path: 'page.png', fullPage: false});
 }
 
 function saveInCsv(productData) {
     let csvContent = '';
 
     // add headers if file not exists
-    if (!fs.existsSync('searched_data.csv')) 
-        csvContent += 'Title,Price,Boughts\n';
-    
+    if (!fs.existsSync('searched_data.csv')) csvContent += 'Title,Price,Boughts\n';
+        
     // add formated data
     csvContent += `"${productData.title || ''}","${productData.price || ''}","${productData.boughts || ''}"\n`;
 
     // create a csv file
     fs.appendFileSync('searched_data.csv', csvContent);
-    console.log('Data successfully saved to CSV file');
+    console.log('\nData successfully saved to CSV file');
 }
 
 main();
